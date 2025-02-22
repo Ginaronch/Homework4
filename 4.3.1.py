48c3d3e33990c1c2cb2308069ff3b3d17a4aca58
Ginaronch <alexandr.yarosh1@gmail.com>
2025-02-22 11:01:46 +0100

    Version 1.0

---
+++ 4.3.1.py
@@ -0,0 +1,3 @@
+import random
+result = range(4, random.randint(3, 10))
+print(result)
--- 4.3.py
+++ 4.3.py
@@ -1,5 +1,5 @@
 import random
-my_list = [random.randrange(10) for element in range(3,10)]
+my_list = [random.randrange(10) for element in range(random.randint(3, 10))] #для каждого элемента в случайном дипазоне от 3 до 10(включительно) выдать случайное значение от 0 до 10(не включительно)
 print(my_list)
 result = [my_list[0], my_list[2], my_list[-2]]
 print(result)
